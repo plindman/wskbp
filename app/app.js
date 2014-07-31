ff79dcfb0cd7a2b3bd4ced2eeac2f7977e9feced
Pontus Lindman <pontus.v.lindman@gmail.com>
2014-07-31 15:31:53 +0300

    some added files committed

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
-'use strict';
-
+(function () {
+  'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('app', [
   'ngRoute',
   'myApp.filters',
   'myApp.services',
@@ -10,7 +10,10 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view1', {templateUrl: 'sampleapp/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: 'sampleapp/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+
+});
